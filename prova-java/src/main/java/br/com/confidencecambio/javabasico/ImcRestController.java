3d543ed85e6084ca08b33fcc4e44267b034e5c5a
alinelsilva <aline-lima-silva@hotmail.com>
2020-10-06 13:52:21 -0300

    imc

--- prova-java/src/main/java/br/com/confidencecambio/javabasico/ImcRestController.java
+++ prova-java/src/main/java/br/com/confidencecambio/javabasico/ImcRestController.java
@@ -1,19 +1,36 @@
 package br.com.confidencecambio.javabasico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.confidencecambio.javabasico.service.HelloService;
+import br.com.confidencecambio.javabasico.service.ImcService;
 
 
 
 @RestController
 public class ImcRestController {
-
-
-//	@RequestMapping(value = "/calcular", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	
+	 private ImcService service;
+
+	    public ImcRestController(final ImcService service) {
+
+	        this.service = service;
+	    }
+
+
+	@RequestMapping(value = "/calculo", method = RequestMethod.GET)	
+	  public ResponseEntity<String> olaMundo(@RequestParam(value = "iCalculo", required = false) double iCalculo) {
+        var retorno = "Seu peso  " + service.calculo(iCalculo);
+        
+        return new ResponseEntity<>(retorno, HttpStatus.OK);
+    }
 
 }
--- prova-java/src/main/java/br/com/confidencecambio/javabasico/service/ImcService.java
+++ prova-java/src/main/java/br/com/confidencecambio/javabasico/service/ImcService.java
@@ -5,8 +5,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class ImcService {
 	
-	public void calculo() {
+	public String calculo(double iCalculo) {
 		
+		String resultado = "";
+		if(iCalculo < 18.5) {
+			resultado = "Abaixo do Peso";
+			
+		} else if(iCalculo >= 18.6 && iCalculo <= 24.9) {
+			resultado ="Saudável";
+			
+		} else if(iCalculo >= 25 && iCalculo <= 29.9) {
+			resultado= "Peso em Excesso";
+			
+		} else if(iCalculo >= 30 && iCalculo <= 34.9) {
+			resultado = "Obesidade Grau I";
+			
+		} else if(iCalculo >= 35 && iCalculo <= 39.9) {
+			resultado = "Obesidade Grau II (severa)";
+			
+		} else {
+			resultado = "Obesidade Grau III (mórbida)";
+		}
+		return resultado;
 		
 	}
 	
